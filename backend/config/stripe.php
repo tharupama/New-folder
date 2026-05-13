44b63d8d0f904ba550273d7abd4452ca2549221e
naveenrasanka <naveenrasanka@gmail.com>
2026-05-14 00:49:34 +0530

    remove key

--- backend/config/stripe.php
+++ backend/config/stripe.php
@@ -1,7 +1,7 @@
 <?php
 
 if (!defined('STRIPE_SECRET_KEY')) {
-    define('STRIPE_SECRET_KEY','sk_test_51TIIzFCmTbH1UkuX7fYkjm5xa6QEgKXqjPzYeen3h7l5hyZNbTSu27udDLHtvmxDhCwrtZ6yqP8HofbTBU6Ei1e600rzul9uiQ');
+    define('STRIPE_SECRET_KEY','');
 }
 
 if (!defined('STRIPE_PUBLISHABLE_KEY')) {
